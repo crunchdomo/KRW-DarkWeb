67bca26f7cd5623e24d986320ab149bb9c376e0c
Adam O <oenfadam@gmail.com>
2024-03-31 13:55:35 +0200

    Version 2 Ontology Init

--- drug-make-onto.py
+++ drug-make-onto.py
@@ -1,4 +1,5 @@
 from rdflib import Graph, Literal, Namespace, URIRef, RDF, RDFS, OWL
+import json
 from urllib.parse import quote
 
 # Initialize the RDF Graph
@@ -143,7 +144,145 @@ Synthetic Marijuana; Time Traveler; Top Gear; Train Wreck; Ultimate; Viper; Vood
 Wizard; Xtreme; Zero Gravity; Zombie"""
 add_drug_with_slang("Synthetic Cannabinoids", meth_slang_terms)
 
-# ... Add more drugs and their slang terms as needed ...
+amphetamines_slang_terms = """Amy; Amps; Bam; B-Bombs; Beans; Bennies; Benz; Black and Whites; Black Beauties; Black Birds; Black
+Bombers; Black Mollies; Blacks; Blue Boys; Bombita; Brain Ticklers; Brownies; Bumblebees; Cartwheels;
+Chalk; Chicken Powder; Chochos; Christina; Chunk; Co-Pilot; Coasts to Coasts; Crisscross; Cross Roads;
+Cross Tops; Debs; Dexies; Diablos; Diamonds; Diet Pills; Dolls; Dominoes; Double Cross; Drivers; Fives;
+Footballs; French Blues; Goofballs; Greenies; Head Drugs; Hearts; Horse Heads; In- Between; Jelly Babies;
+Jelly Beans; Jolly Beans; Jugs; Leapers; Lid Poppers; Lightening; Little Bombs; Marathons; Mini Beans; Mini
+Bennies; Morning Shot; Nuggets; Oranges; Pastillas; Peaches; Pep Pills; Pink Hearts; Pixies; Pollutants;
+Purple Hearts; Rhythm; Rippers; Road Dope; Roses; Rueda; Snaps; Snow Pallets; Sparkle Plenty; Sparklers;
+Speed; Splash; Sweeties; Sweets; Tens; Thrusters; TR-6s; Truck Drivers; Turnabouts; Uppers; Wake Ups;
+West Coast Turnarounds; Wheels; Whiffle Dust; White Crosses; Whites; Zoomers"""
+add_drug_with_slang("Amphetamines", amphetamines_slang_terms)
 
-# Serialize the graph
-g.serialize(destination='DrugOntology.ttl', format='turtle')
+cocaine_slang_terms = """777; A-1; All-American Drug; Angel Powder; Angie; Animals; Audi; Aunt Nora; Azucar; Baby Powder; Barrato;
+Basuco; Bazooka; Beach; Belushi (mixed with heroin); Bernie’s Flakes; Bernie’s Gold Dust; Big Bloke; Big C;
+Big Flake; Big Rush; Billie Hoke; Bird; Birdie Powder; Blanca Nieves; Blanco; Blast; Blizzard; Blonde; Blocks;
+Blow; BMW; Bobo; Bolitas; Bolivian Marching Powder; Bombita (mixed with heroin); Bouncing Powder; Brisa;
+C-Dust; Caca; Cadillac; California Pancakes; Calves; Candy; Car; Carney; Carrie Nation; Cars; Case; Cebolla;
+Cecil; Cement; Charlie; Chevy; Cheyenne; Chinos; Chiva; Clear Kind; Clear Tires; Coca; Coca-Cola; Cocazo;
+Coconut; Coke; Cola; Colorado; Connie; Cookie; Crow; Crusty Treats; Cuadro; Death Valley; Designer
+Jeans; Devil’s Dandruff; Diente; Dienton; Dona Blanca; Double Bubble; Dove; Dream; Dulces; Duracell; Dust;
+Escama; Escorpino; Falopa; Fish (liquid cocaine); Flake; Flea Market Jeans; Florida Snow; Flour; Food;
+Foolish Powder; Fox; Freeze; Friskie Powder; Frula; Gabacho; Galaxy; Gallos; Gato; Gift of the Sun; Gin; Girl;
+Girlfriend; Glad Stuff; Gold Dust; Green Gold; Gringa; Grout; Gueros; Guitar; Hamburger; Happy Dust; Happy
+Powder; Happy Trails; Heaven; Heaven Dust; Henry VIII; Hooter; Hundai; Hunter; Icing; Inca Message; Izzy;
+Jam; Jeep; Jelly; John Deere; Joy Flakes; Joy Powder; Junk; King’s Habit; Kordell; Lady; Lady Snow; Late
+Night; Lavada; Leaf; Line; Loaf; Love Affair; Maca Flour; Mama Coca; Mandango; Maradona; Mayo; Melcocha;
+Mercedes; Milk; Milonga; Mojo; Mona Lisa; Mosquitos; Movie Star Drug; Mujer; Napkin; Nieve; Niña; Nose
+Candy; Nose Powder; Old Lady; Oyster Stew; Paint; Paloma; Palomos; Pantalones; Papas; Paradise;
+Paradise White; Parrot; Pearl; Pedrito; Perico; Peruvian; Peruvian Flake; Peruvian Lady; Pescado; Pez; Pillow;
+Pimp; Pollo; Polvo; Powder; Powder Diamonds; Puritain; Queso Blanco; Racehorse Charlie; Rambo; Refresco; Refrescas; Reindeer Dust; Rims; Rocky Mountain; Rolex; Rooster; Scale; Schmeck; Schoolboy; Scorpion;
+Scottie; Seed; Serpico; Sierra; Shirt; Ski Equipment; Sleigh Ride; Snow; Snow Bird; Snow Cone; Snow White;
+Snowball; Snowflake; Society High; Soda; Soditas; Soft; Space (mixed with PCP); Speedball (mixed with
+heroin); Stardust; Star Spangled Powder; Studio Fuel; Suave; Sugar; Superman; Sweet Stuff; Talco; Talquito;
+Tamales; Taxi; Tecate; Teenager; Teeth; Tequila; Thunder; Tire; Tonto; Toot; Tortes; Toyota; T-Shirts; Turkey;
+Tutti-Frutti; Vaquita; Wash; Wet; Whack (mixed with PCP); White; White Bitch; White Cross; White Girl; White
+Goat; White Horse; White Lady; White Mercedes Benz; White Mosquito; White Paint; White Powder; White
+Root; White Shirt; White T; Whitey; Whiz Bang; Wings; Wooly; Work; Yayo; Yeyo; Yoda; Zip"""
+add_drug_with_slang("Cocaine", cocaine_slang_terms)
+
+crack_slang_terms = """51s; 151s; 501s; Apple Jack; Baby T; Base; Baseball; Bazooka; Beam Me Up; Beautiful Boulders; Beemer; Bill
+Blass; Bings; BJ; Black Rock; Blowcaine; Blowout; Blue; Bobo; Bolo; Bomb; Bone Crusher; Bone; Boo-Boo;
+Boulder; Boy; Breakfast of Champions; Bubble Gum; Bullion; Bump; Candy; Caps; Casper the Ghost; Caviar;
+CD; Cheap Basing; Chewies; Chingy; Clicker; Climax; Cloud; Cloud Nine; Cookies; Crib; Crunch & Munch;
+Devil; Devil Smoke; Dice; Dime Special; Dirty Basing; Dirty Fentanyl (mixed with fentanyl); Double Yoke;
+Durin; Eastside Player; Egg; Eye Opener; Famous Dimes; Fat Bags; Fifty-One; Fish Scales; Freebase; French
+Fries; Garbage Rock; Geek; Glo; Gold; Golf Ball; Gravel; Great White Hope; Grit; Groceries; Hail; Hamburger
+Helper; Hard; Hotcakes; Hubba; Ice; Ice Cubes; Issues; Jelly Beans; Johnson; Kangaroo; Kokoma; Kryptonite;
+Love; Mixed Jive; Moon Rock; Nickel; Nuggets; One-Fifty-One; Paste; Pebbles; Pee Wee; Piedras; Pile; Pony;
+Primo; Quarters; Raw; Ready Rock; Red Caps; RIP (Rest in Peace); Roca; Rock; Rock Attack; Rocks of Hell;
+Rocky III; Rooster; Rox; Roxanne; Roz; Schoolcraft; Scotty; Scramble; Scruples; Seven-Up; Sherms; Sight
+Ball; Slab; Sleet; Smoke; Speed Boat; Square Time Bomb; Stone; Sugar Block; Takeover (mixed with fentanyl);
+Teeth; Tension; Tissue; Top Gun; Troop; Ultimate; Uzi; Wave; White Ball; White Ghost; White Sugar; White
+Tornado; Wrecking Crew; Yahoo; Yale; Yimyom"""
+add_drug_with_slang("Crack Cocaine", crack_slang_terms)
+
+fent_slang_terms = """Apache; Birria (mixed with heroin); Butter; China Girl; China Town; China White; Chinese; Chinese Food;
+Crazy; Crazy One; Dance Fever; Dragon; Dragon’s Breath; Facebook (mixed with heroin in pill form); Fent;
+Fenty; Fire; Friend; Girl; Goodfella; Great Bear; He-Man; Jackpot; King Ivory; Lollipop; Murder 8; Poison;
+Shoes; Tango & Cash; Toe Tag Dope; White Girl"""
+add_drug_with_slang("Fentanyl", fent_slang_terms)
+
+ghb_slang_terms = """G; GEEB; Georgia Home Boy; Grievous Bodily Harm; Gina; Liquid E; Liquid X; Scoop"""
+add_drug_with_slang("GHB", ghb_slang_terms)
+
+hydrocodone_slang_terms = """357s; Bananas; Dro; Fluff; Hydro; Tabs; Norco; Vics; Vikes; Watsons"""
+add_drug_with_slang("Hydrocodone", hydrocodone_slang_terms)
+
+ketamine_slang_terms = """Blind Squid; Cat Valium; Green; Honey Oil; Jet; K; Keller; Kelly’s Day; K-Hold; K-Ways; Special K; Super Acid;
+Vitamin K"""
+add_drug_with_slang("Ketamine", ketamine_slang_terms)
+
+klonopin_slang_terms = """K; K-Pin; Pin; Super Valium; Clonazepam"""
+add_drug_with_slang("Klonopin", klonopin_slang_terms)
+
+marijuana_concentrate_slang_terms = """246; BHO; Badder; Budder; Butter; Dabs; Ear Wax; Errl; Honey Oil; SAP; Shatter; Wax"""
+add_drug_with_slang("Marijuana Concentrates", marijuana_slang_terms)
+
+muschrooms_slang_terms = """Alice; Boomers; Buttons; Caps; Champiñones; Hongos; Magic; Mushies; Pizza Toppings; Shrooms; Tweezes"""
+add_drug_with_slang("Mushrooms", klonopin_slang_terms)
+
+opium_slang_terms = """Auntie; Aunt Emma; Big O; Black; Black Russian (mixed with hashish); Chandoo; China; Chinese Molasses;
+Chinese Tobacco; Chocolate; Cruz; Dopium; Dover’s Powder; Dream Gum; Dream Stick; Dreams; Easing
+Powder; God’s Medicine; Goma; Gondola; Goric; Great Tobacco; Gum; Hocus; Hops; Incense; Joy Plant;
+Midnight Oil; Opio; Pen Yan; Pin Gon; Pin Yen; Pox; Skee; Toxy; Toys; When-Shee; Zero"""
+add_drug_with_slang("Opium", opium_slang_terms)
+
+oxycodone_slang_terms = """30s; 40s; Beans; Blues; Buttons; Greens; OC; Oxy; Whites"""
+add_drug_with_slang("Oxycodone", oxycodone_slang_terms)
+
+pcp_slang_terms = """Ace; Alien Sex Fiend (mixed with heroin); Amoeba; Angel; Angel Dust; Angel Hair; Angel Mist; Angel Poke;
+Animal Tranquilizer; Aurora Borealis; Black Acid (mixed with LSD); Black Whack; Blue Madman; Blue Star;
+Boat; Busy Bee; Butt Naked; Cadillac; Cliffhanger; Columbo; Cozmos; Crazy Coke; Crazy Eddie; Cucuy;
+Cyclones; Detroit Pink; Dipper; Domex (mixed with MDMA); Dummy Dust; Dust; Dust Joint; Dust of Angels;
+Elephant; Elephant Tranquilizer; Embalming Fluid; Energizer; Fake STP; Flakes; Goon; Gorilla Tab; Gorilla
+Biscuits; Green Leaves; Green Tea; Heaven & Hell; Hog; Horse Tracks; Horse Tranquilizers; Jet Fuel; Juice;
+Kaps; K-Blast; Killer; Kools; Leaky Leak; Lemon 714; Lethal Weapon; Love Boat; Mad Dog; Mad Man; Magic
+Dust; Mean Green; Mint Leaf; Mint Weed; Mist; Monkey Dust; Monkey Tranquilizer; New Acid; New Magic;
+Orange Crystal; Ozone; Paz; Peace Pill; Peep; Peter Pan; Pig Killer; Puffy; Purple Rain; Red Devil; Rocket
+Fuel; Rupture; Scuffle; Sheets; Sherms; Shermstick; Space (mixed with cocaine); Spores; Stardust; STP;
+Super Grass; Super Kools; Surfer; Synthetic Cocaine; Taking a Cruise; T-Buzz; Tic Tac; Tish; Trank; Venom;
+Wack (mixed with cocaine); Water; Wet; White Horizon; Wobble Weed; Wolf; Worm; Yellow Fever; Zombie;
+Zoom (mixed with marijuana)"""
+add_drug_with_slang("Oxycodone", oxycodone_slang_terms)
+
+perocset_slang_terms = """512s; Bananas; Blue; Blueberries; Buttons; Ercs; Greenies; Hillbilly Heroin; Kickers; M-30s; Percs; Rims; Tires;
+Wheels"""
+add_drug_with_slang("Percoset", perocset_slang_terms)
+
+peyote_slang_terms = """Black Button; Britton; Button; Cactus; Green Button; Half Moon; Hikori; Hikuli; Hyatari; Nubs; Seni; Shaman;
+Tops"""
+add_drug_with_slang("Peyote", peyote_slang_terms)
+
+lean_slang_terms = """Act; Drank; Lean; Purple; Purple Drank; Sizurup; Sizzurp; Syrup"""
+add_drug_with_slang("Lean", lean_slang_terms)
+
+ritalin_slang_terms = """Kibbles and Bits"""
+add_drug_with_slang("Ritalin", ritalin_slang_terms)
+
+synthetic_cathinones_slang_terms = """Bath Salts; Bliss; Bloom; Blow; Blue Silk; Cloud 9; Drone; Energy-1; Explosion; Flakka (Alpha-PVP); Gravel
+(Alpha-PVP); Insect Repellant; Ivory Wave; Jewelry Cleaner; Lunar Wave; M-Cat; Meow-Meow; Ocean Burst;
+Phone Screen Cleaner; Plant Food; Pure Ivory; Purple Wave; Recharge; Red Dove; Scarface; Snow Leopard;
+Stardust; Vanilla Sky; White Dove; White Knight; White Lightening; White Magic; Zoom"""
+add_drug_with_slang("Synthetic Cathinones", synthetic_cathinones_slang_terms)
+
+xanax_slang_terms = """Bars; Bicycle Handle Bars; Footballs; Hulk; Ladders; Planks; School Bus; Sticks; Xanies; Zanbars; Zannies;
+Z-Bars"""
+add_drug_with_slang("Xanax", xanax_slang_terms)
+
+mescaline_slang_terms = """Big Chief; Blue Caps; Buttons; Cactus; Media Luna; Mescal; Mezcakuba; Moon; San Pedro; Topi"""
+add_drug_with_slang("Mescaline", mescaline_slang_terms)
+
+jsonld_data = g.serialize(format='json-ld')
+
+# Parse the JSON-LD string into a Python dictionary
+jsonld_dict = json.loads(jsonld_data)
+
+# Minify the JSON-LD by dumping the dictionary to a string without whitespace
+minified_jsonld = json.dumps(jsonld_dict, separators=(',', ':'))
+
+# Save the minified JSON-LD string to a file
+with open('DrugOntology.min.jsonld', 'w', encoding='utf-8') as f:
+    f.write(minified_jsonld)
